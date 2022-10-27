6cf02c2796bcfcf30089273bcd4911f24fab84b6
lenovo <lenovo@10.48.77.106>
2022-10-27 10:39:20 +0300

    fourd

--- diziler/src/diziler/arrays.java
+++ diziler/src/diziler/arrays.java
@@ -3,7 +3,16 @@ package diziler;
 public class arrays {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	int[] array= {32,27,64,18,95,14,90,70,60,37};
+	System.out.printf("%s%8s%n", "Index","Value");
+	for(int counter=0; counter<array.length; counter++) {
+		System.out.printf("%5d%8d%n", counter,array[counter]);
+	}
+	System.out.println("Dizinin tersten yazılmıs hali\n");
+	 
+    for(int counter = array.length-1; counter >= 0; counter--){
+        System.out.println("counter  " + (array.length-counter) + ". elemanı = " + (array[counter]));
+    }
 
 	}
 
